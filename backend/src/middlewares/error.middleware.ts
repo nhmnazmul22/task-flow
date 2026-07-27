96139f6534547ecbf84b6bdf74020e06e4da8f23
Nhm Nazmul <najmulmonir12345@gmail.com>
2026-07-27 12:49:18 +0600

    Add custom AppError class and error handling middleware

---
+++ backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,44 @@
+import { AppError } from "@/errors/appError.js";
+import type { NextFunction, Request, Response } from "express";
+import z, { ZodError } from "zod";
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+  }
+
+  if (err instanceof ZodError) {
+    return res.status(400).json({
+      success: false,
+      message: "Validation error",
+      errors: z.flattenError(err).fieldErrors,
+    });
+  }
+
+  if (err.name === "ValidationError") {
+    return res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
+
+  if (err.name === "CastError") {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid ID",
+    });
+  }
+
+  return res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+  });
+}
